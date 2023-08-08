8ae8b69049ae6aa241016bd578995079469c48cb
Esiqee <125314753+Esiqee@users.noreply.github.com>
2023-08-08 23:25:38 +0200

    https://cs50.harvard.edu/python/2022/psets/7/response/

---
+++ CS50/response/response.py
@@ -0,0 +1,18 @@
+from validator_collection import checkers
+
+
+def main():
+    print(validate(input("What's your email address? ")))
+
+
+def validate(s):
+    is_email_address = checkers.is_email(s)
+    if is_email_address == True:
+        return "Valid"
+    if is_email_address == False:
+        return "Invalid"
+
+
+
+if __name__ == "__main__":
+    main()

8d118de9f010c5018fb37643e688b14042abdb70
Esiqee <125314753+Esiqee@users.noreply.github.com>
2023-08-08 15:47:40 +0200

    Create coke.py
    https://cs50.harvard.edu/python/2022/psets/2/coke/

---
+++ CS50/coke/coke.py
@@ -0,0 +1,10 @@
+price = 50
+
+while True :
+    print(f"Amount Due: {price}")
+    x = int(input("Insert Coin: "))
+    if x == 25 or x == 10 or x == 5:
+        price = price - x
+        if price <= 0:
+            break
+print("Change Owed:", price * -1)

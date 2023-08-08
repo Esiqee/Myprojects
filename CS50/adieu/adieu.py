906de70e621ccb95305dca08b96d9506f9e0071f
Esiqee <125314753+Esiqee@users.noreply.github.com>
2023-08-08 15:10:18 +0200

    Create adieu.py
    https://cs50.harvard.edu/python/2022/psets/4/adieu/

---
+++ CS50/adieu/adieu.py
@@ -0,0 +1,20 @@
+kids = []
+
+while True:
+    try:
+        name = input("Name: ")
+        kids.append(name)
+
+    except EOFError:
+        if len(kids) == 1:
+            print(f"Adieu, adieu, to {kids[0]}")
+        elif len(kids) == 2:
+            print(f"Adieu, adieu, to {kids[0]} and {kids[1]}")
+        elif len(kids) > 2:
+            print("Adieu, adieu, to ", end="")
+            for i in range(len(kids) - 1):
+                print(kids[i], end=", ")
+            print(f"and {kids[-1]}")
+        else:
+            print("Error")
+        break
